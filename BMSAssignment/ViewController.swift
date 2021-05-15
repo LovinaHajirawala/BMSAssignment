abe162ef1bd234b1b193120d0be2e0eacee7dce8
Lovina Hajirawala <vhajirawala@gmail.com>
2021-05-15 14:30:30 +0530

    Initial Commit

---
+++ BMSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMSAssignment
+//
+//  Created by Lovina Vijay Hajirawala on 15/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

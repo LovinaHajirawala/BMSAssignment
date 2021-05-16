11cf3fc0127a8dc8c37bb00e76c1079afd6807da
Lovina Hajirawala <vhajirawala@gmail.com>
2021-05-16 09:55:49 +0530

    created storyboard and tableviewcell

---
+++ BMSAssignment/MovieList/ViewController/MovieListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MovieListViewController.swift
+//  BMSAssignment
+//
+//  Created by Lovina Vijay Hajirawala on 15/05/21.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+// end of class
